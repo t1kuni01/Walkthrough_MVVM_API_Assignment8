72675d881d7cc7a9498dd9e59ee9e430281d15c0
Nisha Kumari <t1kuni01@students.oamk.fi>
2023-03-07 12:56:29 +0200

    first commit

---
+++ app/src/main/java/com/example/mvvm_api/ui/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.mvvm_api.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.TabRowDefaults.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mvvm_api.model.Todo
+import com.example.mvvm_api.ui.theme.MVVM_APITheme
+import com.example.mvvm_api.ui.viewmodel.TodoViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MVVM_APITheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    TodoScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TodoScreen(todoViewModel: TodoViewModel = viewModel()) {
+    TodoList(todoViewModel.todos)
+}
+
+@Composable
+fun TodoList(todos: List<Todo>) {
+    LazyColumn(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        items(todos) { todo ->
+            Text(
+                text = todo.title,
+                modifier = Modifier.padding(top = 4.dp, bottom = 4.dp)
+            )
+            Divider(color = Color.LightGray, thickness = 1.dp)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MVVM_APITheme {
+        TodoScreen()
+    }
+}
---
+++ app/src/main/java/com/example/mvvm_api/ui/viewmodel/TodoViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.mvvm_api.ui.viewmodel
+
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mvvm_api.model.Todo
+import com.example.mvvm_api.model.TodosApi
+import kotlinx.coroutines.launch
+
+class TodoViewModel: ViewModel() {
+    var todos  = mutableStateListOf<Todo>()
+        private set
+
+    init {
+        getTodosList()
+    }
+
+    private fun getTodosList(){
+        viewModelScope.launch {
+            var todosApi: TodosApi? = null
+            try {
+                todosApi = TodosApi!!.getInstance()
+                todos.clear()
+                todos.addAll(todosApi.getTodos())
+            } catch (e: Exception)   {
+                Log.d("TODOVIEWMODEL", e.message.toString())
+            }
+        }
+    }
+}
